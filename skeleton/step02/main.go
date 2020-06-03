67c9f47b98cb4447622d3c64747613b4ebc565e6
vagrant <vagrant@homestead>
2020-06-03 08:44:43 +0000

    step04完了

--- skeleton/step02/main.go
+++ skeleton/step02/main.go
@@ -7,7 +7,7 @@ import "fmt"
 
 func main() {
 	// TODO: 品目を入れる変数を定義
-
+	var category string
 	// 値段を入れる変数を定義
 	var price int
 
@@ -17,14 +17,17 @@ func main() {
 	fmt.Scan(&category)
 
 	// TODO: 「値段>」と表示する
+	fmt.Print("値段>")
 	// TODO: 入力した結果をpriceに入れる
-
+	fmt.Scan(&price)
 	// 「===========」と出力して改行する
 	fmt.Println("===========")
 
 	// 品目に「コーヒー」、値段に「100」と入力した場合に
 	// 「コーヒーに100円使いました」と表示する
-	fmt.Printf("%sに%d円使いました\n", category, price)
+	if category == "コーヒー" && price == 100 {
+		fmt.Printf("%sに%d円使いました\n", category, price)
+	}
 
 	// 「===========」と出力して改行する
 	fmt.Println("===========")
